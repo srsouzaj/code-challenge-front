2cd9b3b48932885a728d543280abc7f4f018ff55
Jorge de Souza <jorgedesouzajacintto_jr@hotmail.com>
2025-06-16 20:21:30 -0300

    fix: interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_BASE_URL });
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (e) => {
+    return Promise.reject(e);
+  }
+);
 
 export default api;
